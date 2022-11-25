81b340afa1ff8b2fb36c2987dadbc44f1ba95a25
Ori <orinurieli1@gmail.com>
2022-11-25 16:37:48 +0200

    b

--- User.cpp
+++ User.cpp
@@ -20,7 +20,6 @@ User::User(const char* name, Date birthday)
 	_likedPages = new Page * [1];
 	_friendsList = new User * [_maxNumOfFriends]; // array of pointers so it wont go through c'tor
 
-	///////////////////////////////////////////////////////////////////////////////////////////
 	// for debugging:
 	cout << "name: " << _name << endl;
 	cout << "number of friends: " << _numOfFriends << endl;
